d33524e39419465f9885c12e785cfcb15715b33e
Kevikajyothi <jyothikevika2@gmail.com>
2023-06-26 15:04:59 +0530

    test

---
+++ one.js
@@ -0,0 +1,14 @@
+let a =[0,1,2,3,4,5]
+
+// console.log(typeof(a))
+// console.log(a)
+// console.log(a[0]);
+// console.log(a.length);
+var i=0
+
+while(i<a.length){
+    if(a[i]%2!=0){
+        console.log(a[i]);
+    }
+    i=i+1
+}
